e11ff4ce34e38deaf6aef917ed5913b94a5a484d
Mukti Wibowo <muktiw7@gmail.com>
2021-01-06 00:45:32 +0700

    1. Fixed question update issues 2. Changed array_map to foreach since they didn't return any data 3. Use Request instead of each url var to prevent overpopulating args

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -35,7 +35,7 @@ class ExamController extends Controller
       $caseStudy = CaseStudy::create($fields);
 
       if ($request->level_id) {
-        $caseStudy->leve_id = $request->level_id;
+        $caseStudy->level_id = $request->level_id;
         $caseStudy->save();
       }
 
@@ -50,24 +50,23 @@ class ExamController extends Controller
       // Question
       $qFields = $request->only('body', 'type', 'essay', 'score');
       $qFields['id'] = Str::uuid();
+      $qFields['level_id'] = $request->level_id;
 
       $level = Level::find($request->level_id);
       $lastQuestionAdded = $level->questions()->orderBy('number', 'desc')->first();
       $qFields['number'] = !$lastQuestionAdded ? 1 : $lastQuestionAdded->number + 1;
 
       $question = Question::create($qFields);
-      $question->level()->associate($level);
       
       if ($request->case_study_id) {
         $question->case_study_id = $request->case_study_id;
+        $question->save();
       }
 
-      $question->save();
-
-      if ($question->type !== 'ESSAY') {
+      if ($request->items) {
         // Question Items (Multiple Choice or Checklist)
-        array_map(function ($items) use ($question) {
-          $iFields = (array) json_decode($items);
+        foreach ($request->items as $item) {
+          $iFields = (array) json_decode($item);
           $iFields['id'] = Str::uuid();
           
           $model = $question->type == 'MULTIPLE' ? Choice::class : Checklist::class;
@@ -75,7 +74,7 @@ class ExamController extends Controller
           $item = $model::create($iFields);
           $item->question()->associate($question);
           $item->save();
-        }, $request->items);
+        }
       }
       
       return res('Successfully created question', $question, 201);
@@ -84,11 +83,11 @@ class ExamController extends Controller
     return catcher($fn);
   }
 
-  public function updateLevels ($id, Request $request) {
-    $fn = function () use ($id, $request) {
+  public function updateLevels (Request $request) {
+    $fn = function () use ($request) {
       $fields = $request->only('name', 'tujuan', 'uraian', 'examThreshold', 'evaluationThreshold');
 
-      $level = Level::find($id);
+      $level = Level::find($request->level_id);
       
       $level->update($fields);
 
@@ -98,19 +97,13 @@ class ExamController extends Controller
     return catcher($fn);
   }
   
-  public function updateCaseStudies ($id, Request $request) {
-    $fn = function () use ($id, $request) {
+  public function updateCaseStudies (Request $request) {
+    $fn = function () use ($request) {
       $fields = $request->only('number', 'title', 'body', 'type');
 
-      $caseStudy = CaseStudy::find($id);
+      $caseStudy = CaseStudy::find($request->case_study_id);
 
       $caseStudy->update($fields);
-
-      if ($request->level_id && $request->level_id != $caseStudy->level_id) {
-        
-        $caseStudy->level_id = $request->level_id;
-        $caseStudy->save();
-      }
 
       return res('Successfully updated case study', $caseStudy, 201);
     };
@@ -118,20 +111,21 @@ class ExamController extends Controller
     return catcher($fn);
   }
 
-  public function updateQuestions ($id, Request $request) {
-    $fn = function () use ($id, $request) {
+  public function updateQuestions (Request $request) {
+    $fn = function () use ($request) {
       // Question
       $qFields = $request->only('body', 'essay', 'score');
-      $question = Question::find($id);
+      $question = Question::find($request->question_id);
 
-      if ($request->case_study_id !== $question->case_study_id) {
+      if ($request->case_study_id && $request->case_study_id !== $question->case_study_id) {
         $question['case_study_id'] = 'NONE' ? null : $request->case_study_id;
       }
 
       $question->update($qFields);
       
-      // Question Items
+      // Upcoming question type is different from old one
       if ($request->type !== $question->type) {
+        // Cleaning previous question items
         switch ($request->type) {
           case 'ESSAY':
             if ($question->type === 'MULTIPLE') {
@@ -153,26 +147,41 @@ class ExamController extends Controller
             }
             break;
         }
-
-        $question->type = $request->type;
-        $question->save();
-
-        if ($request->type !== 'ESSAY') {
+        
+        // Create new question items
+        if ($request->items) {
           $question->essay = null;
-          $question->save();
-
-          array_map(function ($items) use ($question) {
-            $iFields = (array) json_decode($items);
+  
+          foreach ($request->items as $item) {
+            $iFields = (array) json_decode($item);
             $iFields['id'] = Str::uuid();
             
-            $model = $question->type == 'MULTIPLE' ? Choice::class : Checklist::class;
+            $model = $request->type == 'MULTIPLE' ? Choice::class : Checklist::class;
     
             $item = $model::create($iFields);
             $item->question()->associate($question);
             $item->save();
-          }, $request->items);
+          }
+        }
+      } else {
+        // Upcoming question type is the same as the old one
+        // Update question items with new data
+        if ($request->items) {
+          $items = $question->type === 'MULTIPLE' ? $question->choices() : $question->checklists();
+          $order = $question->type === 'MULTIPLE' ? 'point' : 'number';
+          
+          $items = $items->orderBy($order, 'asc')->get();
+
+          foreach ($items as $key => $item) {
+            $iFields = (array) json_decode($request->items[$key]);
+
+            $item->update($iFields);
+          }
         }
       }
+      
+      $question->type = $request->type;
+      $question->save();
 
       return res('Successfully updated question', $question, 201);
     };
@@ -180,9 +189,9 @@ class ExamController extends Controller
     return catcher($fn);
   }
 
-  public function deleteLevels ($id) {
-    $fn = function () use ($id) {
-      Level::destroy($id);
+  public function deleteLevels (Request $request) {
+    $fn = function () use ($request) {
+      Level::destroy($request->level_id);
 
       return res('Successfully deleted level', null, 204);
     };
@@ -190,9 +199,9 @@ class ExamController extends Controller
     return catcher($fn);
   }
 
-  public function deleteCaseStudies ($id) {
-    $fn = function () use ($id) {
-      $caseStudy = CaseStudy::find($id);
+  public function deleteCaseStudies (Request $request) {
+    $fn = function () use ($request) {
+      $caseStudy = CaseStudy::find($request->case_study_id);
 
       // Disassociate children, which in this case Questions
       $caseStudy->questions()->transform(function ($question) {
@@ -210,11 +219,46 @@ class ExamController extends Controller
     return catcher($fn);
   }
 
-  public function deleteQuestions ($id) {
-    $fn = function () use ($id) {
-      Question::destroy($id);
+  public function deleteQuestions (Request $request) {
+    $fn = function () use ($request) {
+      Question::destroy($request->question_id);
 
       return res('Successfully deleted question', null, 204);
+    };
+    
+    return catcher($fn);
+  }
+
+  public function allQuestions (Request $request) {
+    $fn = function () use ($request) {
+      $questions = Level::find($request->level_id)
+                        ->questions()
+                        ->select('id', 'level_id', 'number', 'body', 'type', 'created_at', 'updated_at')
+                        ->orderBy('number')
+                        ->get();
+
+      return res('Success.', $questions);
+    };
+    
+    return catcher($fn);
+  }
+
+  public function showQuestions (Request $request) {
+    $fn = function () use ($request) {
+      $question = Question::find($request->question_id);
+
+      if ($question->type !== 'ESSAY') {
+        $opt = [
+          'items' => $question->type === 'MULTIPLE' ? 'choices' : 'checklists',
+          'order' => $question->type === 'MULTIPLE' ? 'point' : 'number'
+        ];
+        
+        $question->load([$opt['items'] => function ($query) use ($opt) {
+          $query->orderBy($opt['order']);
+        }]);
+      }
+
+      return res('Success.', $question);
     };
     
     return catcher($fn);
