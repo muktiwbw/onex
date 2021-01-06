730cc2b66af2a46696798f0fa076b0620b626c22
Mukti Wibowo <muktiw7@gmail.com>
2021-01-06 16:54:11 +0700

    Added non-essay answer calculation

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -14,12 +14,12 @@ class AssessmentController extends Controller
   public function allAnswerSheets (Request $request) {
     $fn = function () use ($request) {
       $answerSheets = Level::find($request->level_id)
-                          ->answerSheets()
-                          ->select('id', 'updated_at as finished_at','user_id')
-                          ->where('isFinished', true)
-                          ->orderBy('updated_at', 'desc')
-                          ->with('user:id,name')
-                          ->get();
+                            ->answerSheets()
+                            ->select('id', 'updated_at as finished_at','user_id')
+                            ->where('isFinished', true)
+                            ->orderBy('updated_at', 'desc')
+                            ->with('user:id,name')
+                            ->get();
 
       return res('Success.', $answerSheets);
     };
@@ -31,8 +31,66 @@ class AssessmentController extends Controller
     $fn = function () use ($request) {
       $answerSheet = AnswerSheet::find($request->answer_sheet_id);
 
-      // Calculate non-essay answers
-      // ...
+      // Calculate multiple choice answers
+      $multipleChoiceAnswers = $answerSheet
+                              ->answers()
+                              ->whereNotNull('point')
+                              ->get()
+                              ->map(function ($answer) {
+                                $correctPoint = $answer->question
+                                                       ->choices()
+                                                       ->where('isCorrect', true)
+                                                       ->first()->point;
+                                return [
+                                  'number' => $answer->question->number,
+                                  'score' => $answer->point === $correctPoint ? $answer->question->score : 0,
+                                  'submittedAnswer' => $answer->point,
+                                  'correctAnswer' => $correctPoint
+                                ];
+                              });
+      
+      $multipleChoiceFinalScore = $multipleChoiceAnswers
+                                  ->map(function ($choice) {
+                                    return $choice['score'];
+                                  })->sum();
+
+      // Calculate checklist answers
+      $checklistAnswers = $answerSheet
+                         ->answers()
+                         ->whereNotNull('checklist')
+                         ->get()
+                         ->map(function ($answer) {
+                           $checklistQuestionScore = $answer->question->score;
+                           $checklistItemsCount = $answer->question->checklists->count();
+                           $checklistScorePerItem = $checklistQuestionScore / $checklistItemsCount;
+                           
+                           $checklistItems = $answer->question
+                                                    ->checklists()
+                                                    ->orderBy('number', 'asc')
+                                                    ->get();
+
+                           $submittedChecklistItems = collect(json_decode($answer->checklist))
+                                                      ->map(function ($checklist, $key) use ($checklistItems, $checklistScorePerItem) {
+                                                        
+                                                        return [
+                                                          'number' => $checklist->number,
+                                                          'score' => $checklist->answer === $checklistItems[$key]->answer ? $checklistScorePerItem : 0,
+                                                          'submittedAnswer' => $checklist->answer,
+                                                          'correctAnswer' => $checklistItems[$key]->answer
+                                                        ];
+                                                      });
+                                                      
+                           return [
+                             'number' => $answer->question->number,
+                             'checklist' => json_encode($submittedChecklistItems),
+                             'score' => $submittedChecklistItems
+                                        ->map(function ($item) {
+                                          return $item['score'];
+                                        })->sum()
+                           ];
+                         });
+
+      // dd($checklistAnswers[0]);        
     };
     
     return catcher($fn);
